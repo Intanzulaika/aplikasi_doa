bfd311a1d19915f67dd5d7c075deb67085a136b9
Intanzulaika <intanzulaika2001@gmail.com>
2021-09-20 22:36:49 +0800

    first commit

---
+++ assets/foto/daftarmakan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DaftarMakan extends StatefulWidget {
+  @override
+  _DaftarMakanState createState() => _DaftarMakanState();
+}
+
+class _DaftarMakanState extends State<DaftarMakan> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
